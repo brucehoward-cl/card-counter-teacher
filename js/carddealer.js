22c4ee68ab835a074eff3948e5046b4abcec612d
Bruce Howard <brucehoward@twc.com>
2020-06-05 21:56:57 -0400

    Added new game and hit me buttons

--- js/carddealer.js
+++ js/carddealer.js
@@ -1,14 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {});
 
-    const dlrCard2 = document.getElementById('dlrCard2');
 
     const btnHitMe = document.getElementById('btnhitme');
-    const btnstay = document.getElementById('btnstay');
+    const btnHitDlr = document.getElementById('btnhitdlr');
+    const btnNewGame = document.getElementById('btnnewgame');
     const form = document.getElementById('registrar');
 
+    btnHitMe.addEventListener("click", hitMe);
+    btnHitDlr.addEventListener("click", hitDealer);
+    btnNewGame.addEventListener("click", newGame);
     
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+
+     function hitDealer() {
         let dlrCard1 = document.getElementById('dlrCard1');
         if (dlrCard1.textContent == 'This is the first card') {
             dlrCard1.textContent = 'Ace of Spade';
@@ -24,6 +27,39 @@ document.addEventListener('DOMContentLoaded', () => {
             sum += 10;
             dlrSum.textContent = sum;
        }
-    });
+    }
 
-});
+    function hitMe() {
+        let plyrCard1 = document.getElementById('plyrCard1');
+        if (plyrCard1.textContent == 'This is the first card') {
+            plyrCard1.textContent = 'Ace of Hearts';
+            let plyrSum = document.getElementById('plyrSum');
+            var sum = parseInt(plyrSum.textContent);
+            sum += 11;
+            plyrSum.textContent = sum;
+        }
+        else {
+            plyrCard2.textContent = 'Jack of Hearts';
+            let plyrSum = document.getElementById('plyrSum');
+            var sum = parseInt(plyrSum.textContent);
+            sum += 10;
+            plyrSum.textContent = sum;
+       }
+    }
+
+    function newGame() {
+        let plyrCard1 = document.getElementById('plyrCard1');
+        let plyrCard2 = document.getElementById('plyrCard2');
+        let dlrCard1 = document.getElementById('dlrCard1');
+        let dlrCard2 = document.getElementById('dlrCard2');
+        let plyrSum = document.getElementById('plyrSum');
+        let dlrSum = document.getElementById('dlrSum');
+        plyrCard1.textContent = 'Card 1';
+        plyrCard2.textContent = 'Card 2';
+        dlrCard1.textContent = 'Card 1';
+        dlrCard2.textContent = 'Card 2';
+        plyrSum.textContent = '0';
+        dlrSum.textContent = '0';
+    }
+
+
